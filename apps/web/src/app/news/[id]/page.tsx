0ca885b198e5de0801a3767f7c0fc37699a2024f
Ilya Stepanov <moolcoov@gmail.com>
2023-11-13 21:42:19 +0800

    📄 metadata

--- apps/web/src/app/news/[id]/page.tsx
+++ apps/web/src/app/news/[id]/page.tsx
@@ -1,12 +1,37 @@
 import cn from "classnames";
 import { Suspense } from "react";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { Post, PostFallback } from "./sections/Post";
 import { PostInfo } from "./sections/PostInfo";
 import styles from "./PostPage.module.scss";
 import { BackButton } from "./sections/BackButton";
 import { PostInfoFallback } from "./sections/PostInfo/PostInfo.fallback";
+import { VkWallGetById } from "@/lib/vk";
 
-export default function NewsPostPage({ params }: { params: { id: string } }): JSX.Element {
+interface PageProps {
+	params: { id: string };
+	searchParams: Record<string, string | string[] | undefined>;
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+	const postResponse: VkWallGetResponse = await VkWallGetById({ id: params.id });
+	const post: VkWallGetPost | undefined = postResponse.response.items.at(0);
+
+	if (!post) {
+		notFound();
+	}
+
+	const image = getImage(post.attachments, post.copy_history);
+	const title = getText(post.text, Boolean(image), post.copy_history);
+
+	return {
+		title: title.length >= 40 ? `${title.slice(0, 40)}...` : title,
+		robots: "noindex, nofollow",
+	};
+}
+
+export default function NewsPostPage({ params }: PageProps): JSX.Element {
 	return (
 		<>
 			<div className={cn("wrapper", styles["back-button"])}>
@@ -22,4 +47,45 @@ export default function NewsPostPage({ params }: { params: { id: string } }): JS
 			</div>
 		</>
 	);
+}
+
+function getText(postText: string, hasImage: boolean, copyHistory?: VkWallGetPost[]): string {
+	if (postText) {
+		return postText;
+	}
+
+	const copyHistoryText = copyHistory?.at(0)?.text;
+	if (copyHistoryText !== "" && copyHistoryText) {
+		return copyHistoryText;
+	}
+
+	if (hasImage) {
+		return "🖼️ Картинка";
+	}
+
+	return "";
+}
+
+function getImage(
+	attachments?: VkWallGetPostAttachments,
+	copyHistory?: VkWallGetPost[],
+): VkWallGetPostAttachmentPhoto | undefined {
+	if (attachments) {
+		for (const attachment of attachments) {
+			if (attachment.type === "photo") {
+				return attachment;
+			}
+		}
+	}
+
+	if (copyHistory) {
+		for (const post of copyHistory) {
+			const image = getImage(post.attachments, undefined);
+			if (image) {
+				return image;
+			}
+		}
+	}
+
+	return undefined;
 }
